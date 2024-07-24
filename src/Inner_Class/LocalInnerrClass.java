750573c5af1a03e36006b62ceece5171e489b703
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-07-24 13:49:37 +0530

    Added Implementation For LocalInnerrClass

--- src/Inner_Class/LocalInnerrClass.java
+++ src/Inner_Class/LocalInnerrClass.java
@@ -1,4 +1,20 @@
 package Inner_Class;
 
 public class LocalInnerrClass {
+
+    public void localInnerClassMethod(){
+
+        class ActualInnerClass{
+
+            public void display(){
+                System.out.println("Inner Class Message is Displayed...");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LocalInnerrClass aClass = new LocalInnerrClass() ;
+
+    }
 }
