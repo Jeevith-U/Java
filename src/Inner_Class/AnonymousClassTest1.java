846e8e5e0ca80d2398ff32b9081f75625dd7ef63
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-07-24 17:26:06 +0530

    Implemented Added for static inner class

--- src/Inner_Class/AnonymousClassTest1.java
+++ src/Inner_Class/AnonymousClassTest1.java
@@ -5,19 +5,3 @@ public abstract class AnonymousClassTest1 {
     public abstract void testAbstractMethod1();
 
 }
-
-//class ImpAnonymousClassTest1 extends  AnonymousClassTest1{
-//
-//    public void creationOfAnonymousClass(){
-//
-//        AnonymousClassTest1 obj = new AnonymousClassTest1(){
-//
-//            public void testAbstractMethod1() {
-//
-//                System.out.println("Some Implementation");
-//            }
-//        }
-//
-//    }
-//
-//}
